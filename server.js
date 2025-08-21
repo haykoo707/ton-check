44f6f0d5f0a61a31a7c6f24dfff173b383260b2c
Hayko <hayk.ghukasyannnn@gmail.com>
2025-08-21 23:13:28 +0400

    Update server.js

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.use((req, res, next) => {
   next();
 });
 
-const RECEIVER = "UQCb8i7V_2QxUurP0ZCIHCVglCmKSeKjIzgHekP3XDondvbm";
+const RECEIVER = "UQBfrU75WGhBLnRpBs1ImWE5sPdxKsFMBgogpD578JxXyDbK";
 
 // Package configurations that match your HTML
 const PACKAGES = {
